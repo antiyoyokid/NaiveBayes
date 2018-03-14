4db7503fc5ce3e3a1799d1667f2095456aa1c454
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-03-14 00:45:23 -0500

    First Commmit. Need to work on Code reader and get in the data

---
+++ src/DataReader.cpp
@@ -0,0 +1,26 @@
+//
+// Created by aishi on 3/14/2018.
+//
+
+#include "DataReader.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+#include <limits>
+
+int main()
+{
+   std::ifstream file("C:\\Users\\aishi\\CLionProjects\\naivebayes-antiyoyokid\\data\\digitdata\\trainingimages.txt");
+    std::string str;
+    while (std::getline(file, str))
+    {
+        //make a vector 
+    }
+}
+
+
+
+
+
+
+
